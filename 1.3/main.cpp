77bb26a86e15023d55c972d07ef5036945e9618f
Victoria Brovina <117751176+vioviooo@users.noreply.github.com>
2024-10-10 17:39:03 +0300

    main 1.3 final touches

--- 1.3/main.cpp
+++ 1.3/main.cpp
@@ -21,11 +21,30 @@ void print_matrix(const std::vector<std::vector<double>> &matrix) {
     std::cout << '\n';
 }
 
-// * функция подсчета нормы разности двух векторов
+bool is_diagonally_dominant(const std::vector<std::vector<double>> &matrix) {
+    size_t n = matrix.size();
+    for (size_t i = 0; i < n; ++i) {
+        double sum_d = std::abs(matrix[i][i]);
+        double sum = 0.0;
+
+        for (size_t j = 0; j < n; ++j) {
+            if (i != j) {
+                sum += std::abs(matrix[i][j]);
+            }
+        }
+
+        if (sum_d <= sum) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// * функция подсчета нормы разности двух векторов (L-infinity norm)
 double calculate_norm(const std::vector<double> &x, const std::vector<double> &prev_x) {
     double norm = 0.0;
     for (size_t i = 0; i < x.size(); ++i) {
-        norm = std::max(norm, fabs(x[i] - prev_x[i]));
+        norm = std::max(norm, std::abs(x[i] - prev_x[i]));
     }
     return norm;
 }
@@ -35,7 +54,7 @@ std::vector<double> simple_iterations_algorithm(const std::vector<std::vector<do
     size_t n = matrix.size();
     // * начальное приближение
     std::vector<double> x(n, 0.0);
-    std::vector<double> prev_x(n);
+    std::vector<double> prev_x(n, 0.0);
 
     do {
         prev_x = x;
@@ -46,9 +65,14 @@ std::vector<double> simple_iterations_algorithm(const std::vector<std::vector<do
                     sum += matrix[i][j] * prev_x[j];
                 }
             }
+            if (matrix[i][i] == 0) {
+                throw std::logic_error("* Деление на ноль\n");
+            }
             x[i] = (b[i] - sum) / matrix[i][i];
         }
         ++iter;
+        double norm = calculate_norm(x, prev_x);
+        std::cout << std::fixed << std::setprecision(10)<< "Simple iterations norm: " << norm << '\n';
     } while (calculate_norm(x, prev_x) > EPS);
     
     return x;
@@ -70,10 +94,17 @@ std::vector<double> seidel_algorithm(const std::vector<std::vector<double>> &mat
             for (size_t j = i + 1; j < n; ++j) {
                 sum += matrix[i][j] * prev_x[j];
             }
+            if (matrix[i][i] == 0) {
+                throw std::logic_error("* Деление на ноль\n");
+            }
             x[i] = (b[i] - sum) / matrix[i][i];
         }
         ++iter;
-        if (calculate_norm(x, prev_x) <= EPS) {
+
+        double norm = calculate_norm(x, prev_x);
+        std::cout << std::fixed << std::setprecision(10) << "Seidel algorithm norm: " << norm << '\n';
+        
+        if (norm <= EPS) {
             break;
         }
     } while (true);
@@ -144,6 +175,11 @@ int main() {
         }
     }
 
+    if (!is_diagonally_dominant(matrix)) {
+        std::cout << "* Матрица не является диагонально доминирующей.\n";
+        return 1;
+    }
+
     std::vector<double> b(n, 0.0);
     for (int i = 0; i < n; ++i) {
         fin >> b[i];
@@ -156,12 +192,25 @@ int main() {
     int simple_iter = 0;
     int seidel_iter = 0;
 
-    std::vector<double> simple_iter_result = simple_iterations_algorithm(matrix, b, EPS, simple_iter);
+    std::vector<double> simple_iter_result;
+    try {
+       simple_iter_result = simple_iterations_algorithm(matrix, b, EPS, simple_iter);
+    } catch(std::exception &e) {
+        std::cout << e.what() << '\n';
+        return 1;
+    }
+
     std::cout << "> Решение методом простых итераций:" << '\n';
     print_matrix(simple_iter_result);
     std::cout << "> Количество итераций: " << simple_iter << '\n';
 
-    std::vector<double> gauss_seidel_result = seidel_algorithm(matrix, b, EPS, seidel_iter);
+    std::vector<double> gauss_seidel_result;
+    try {
+        gauss_seidel_result = seidel_algorithm(matrix, b, EPS, seidel_iter);
+    } catch(std::exception &e) {
+        std::cout << e.what() << '\n';
+        return 1;
+    }
     std::cout << "\n> Решение методом Зейделя:" << '\n';
     print_matrix(gauss_seidel_result);
     std::cout << "> Количество итераций: " << seidel_iter << '\n';
